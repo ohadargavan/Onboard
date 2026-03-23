fdac76327eb75857947a545ffa00b57026e6bd3b
ohadargavan <ohad.argavan@gmail.com>
2026-03-23 15:59:27 +0200

    Endpoints 1, 5

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Onboard Admissions API")
 
 admissions_service = Service("flow.json")
 
-# Pydantic Models for incoming info
+# ---Pydantic Models for incoming info---
 
 class CreateUserRequest(BaseModel):
     # Endpoint 1: POST request
@@ -21,3 +21,17 @@ class SubmitTaskRequest(BaseModel):
     step_id: str
     task_id: str
     task_payload: Dict[str, Any]
+
+
+# Endpoint 1 (POST)
+@app.post("/users")
+def create_user_endpoint(request: CreateUserRequest):
+
+    new_user_id = admissions_service.create_user(request.email)
+    return {"user_id": new_user_id}
+
+# EndPoint 5 (GET)
+@app.get("/users/{user_id}/status")
+def get_user_status_endpoint (user_id: str):
+    user_status = admissions_service.get_user_status(user_id)
+    return {"user_status" : user_status}
